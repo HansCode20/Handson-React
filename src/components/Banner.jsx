5e930aa841aa7c7fc68b6905b09758e46e37209c
HansCode20 <fananiilham20@gmail.com>
2024-02-11 00:32:33 +0700

    nambah Banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img src="src/assets/Image.jpg" alt="image"  />
+    </div>
+  );
+};
+
+export default Banner;
