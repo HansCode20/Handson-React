636d8a1ce8b18742b4465f48de4550528c199f2a
HansCode20 <fananiilham20@gmail.com>
2024-02-11 00:36:07 +0700

    Menguba File

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const name = "HAM";
-  const isSingle = false;
+  // const name = "HAM";
+  // const isSingle = false;
 
-  const renderStatus = (status) => {
-    if (status) {
-      return <p>Saya sudah menikah</p>;
-    } else {
-      return <p>Saya belum menikah</p>;
-    }
-  };
+  // const renderStatus = (status) => {
+  //   if (status) {
+  //     return <p>Saya sudah menikah</p>;
+  //   } else {
+  //     return <p>Saya belum menikah</p>;
+  //   }
+  // };
 
   return (
     <div>
       <Navbar />
-      <h1>HELLO REACT {name}</h1>
+      <Banner />
+      <Footer />
+      {/* {<h1>HELLO REACT {name}</h1>
       {isSingle ? <p>Saya sudah menikah</p> : <p>Saya belum menikah</p>}
-      {renderStatus(isSingle)}
+      {renderStatus(isSingle)}} */}
     </div>
   );
 };
